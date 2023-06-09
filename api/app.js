a1588b9c3f8cc8d9a932fd77aadec7149808e450
Emma <emma.gaucher@gmaiL.com>
2023-06-09 20:12:15 +0200

    route creation

--- api/app.js
+++ api/app.js
@@ -4,9 +4,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+
+const cors = require('cors');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +17,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const { Client } = require('pg');
 
-/* GET home page. */
+const connectionString = 'postgres://oaycmzji:OzdnG1NOGN7aGZbXuYI10Y6l0F10zC4w@lucky.db.elephantsql.com/oaycmzji';
+
+const client = new Client({ connectionString });
+client.connect();
+
+// - Quand on arrive sur la homepage, on renvoi un JSOn vide
+// - Quand tu vas sur /trips en GET il faut que ça renvoi du SQL
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ test: true });
+});
+
+router.get('/trips', (req, res) => {
+  // req.query.departure
+  client.query('SELECT * FROM public.tickets ORDER BY id ASC', [departure.query.departure, arrival.query.arrival] )
+  .then(data => {
+    res.json(data.rows);
+  })
+  .catch(error => {
+    res.json({ error: true, message: error });
+  })
+});
+
+router.get("*", (req, res) => {
+  res.json({ error: 'page not found' })
 });
 
 module.exports = router;
+
